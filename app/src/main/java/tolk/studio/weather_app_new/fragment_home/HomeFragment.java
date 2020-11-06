677b87bc1da6e4f48c790b54f36e42f9f420b109
Aleksey Stolkov <AlexeyStolkov@gmail.com>
2020-11-06 21:08:42 +0300

    New app Weather

---
+++ app/src/main/java/tolk/studio/weather_app_new/fragment_home/HomeFragment.java
@@ -0,0 +1,33 @@
+package tolk.studio.weather_app_new.fragment_home;
+
+import android.content.res.TypedArray;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import tolk.studio.weather_app_new.R;
+
+public class HomeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        
+
+    }
+}
